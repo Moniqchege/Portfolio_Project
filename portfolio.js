7c259b87245a19368aba40213460d437c9f242df
Moniqchege <quichege33@gmail.com>
2024-04-04 13:31:25 +0300

    portfolio_project

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-  strings: ["Frontend Developer", "UI/UX Designer", "Web Developer"],
+  strings: ["Frontend Developer", "React Developer", "Web Designer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
